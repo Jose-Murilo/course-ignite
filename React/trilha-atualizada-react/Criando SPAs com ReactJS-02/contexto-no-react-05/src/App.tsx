190b7fefa155e5585da495fcc4bd9822deb47ed9
Jose-Murilo <murilojucas360@hotmail.com>
2023-06-17 16:27:23 -0300

    added part of Context (still buggy!)

---
+++ React/trilha-atualizada-react/Criando SPAs com ReactJS-02/contexto-no-react-05/src/App.tsx
@@ -0,0 +1,22 @@
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+// Para explicação
+// import { ContextExample } from './ContextExample'
+
+export function App() {
+  // Para explicação
+  // return <ContextExample />
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
