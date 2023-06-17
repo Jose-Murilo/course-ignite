80ddc33b63d7d6a09c60bb44c0167af03c815bec
Jose-Murilo <murilojucas360@hotmail.com>
2023-06-17 00:15:17 -0300

    added starting a new cycle

--- React/trilha-atualizada-react/Criando SPAs com ReactJS-02/funcionalidades-da-aplicacao-04/src/pages/Home/index.tsx
+++ React/trilha-atualizada-react/Criando SPAs com ReactJS-02/funcionalidades-da-aplicacao-04/src/pages/Home/index.tsx
@@ -12,6 +12,7 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
+import { useState } from 'react'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -23,7 +24,17 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  isActive: boolean
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -33,12 +44,26 @@ export function Home() {
   })
 
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data)
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      isActive: false,
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
     reset()
   }
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
   const task = watch('task')
   const isSubmitDisabled = !task
+
+  console.log(activeCycle)
 
   return (
     <HomeContainer>
